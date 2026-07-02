2b8ee4c05d6760f4d047c7b53db535f7ea5e56cc
Eduardo Pereira <doardoperera@gmail.com>
2026-07-01 22:53:54 -0300

    feat: adiciona função para extração com parâmetros de paths

---
+++ src/scripts/extract.py
@@ -0,0 +1,20 @@
+import zipfile
+from pathlib import Path
+
+
+# Função para extrair dados de um arquivo compactado e salvar em outro local.
+def extrair_dados(path_origem: str, path_destino: str):
+
+    if not Path(path_origem).exists():
+        raise FileNotFoundError(
+            f"O arquivo de origem '{path_origem}' não foi encontrado."
+        )
+
+    Path(path_destino).parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        with zipfile.ZipFile(path_origem, "r") as zip_ref:
+            zip_ref.extractall(Path(path_destino).parent)
+
+    except Exception as e:
+        raise RuntimeError(f"Erro ao extrair dados do arquivo '{path_origem}': {e}")
